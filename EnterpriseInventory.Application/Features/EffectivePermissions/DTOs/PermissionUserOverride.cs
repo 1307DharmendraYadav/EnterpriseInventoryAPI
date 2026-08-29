6f877b192a4a196adf0e484fd44d7a63ede46867
Dharmendra Yadav <dharmendra.adv92@gmail.com>
2026-08-29 07:57:32 +0530

    feat(auth): implement permission breakdown API

---
+++ EnterpriseInventory.Application/Features/EffectivePermissions/DTOs/PermissionUserOverride.cs
@@ -0,0 +1,8 @@
+﻿namespace EnterpriseInventory.Application.Features.EffectivePermissions.DTOs;
+
+public sealed class PermissionUserOverride
+{
+    public bool IsAllowed { get; init; }
+
+    public string OverrideType { get; init; } = string.Empty;
+}
---
+++ EnterpriseInventory.Application/Features/EffectivePermissions/Interfaces/IPermissionBreakdownService.cs
@@ -0,0 +1,10 @@
+﻿using EnterpriseInventory.Application.Features.EffectivePermissions.DTOs;
+
+namespace EnterpriseInventory.Application.Features.EffectivePermissions.Interfaces;
+
+public interface IPermissionBreakdownService
+{
+    Task<PermissionBreakdownResponse> GetPermissionBreakdownAsync(
+        int userId,
+        int permissionId);
+}
